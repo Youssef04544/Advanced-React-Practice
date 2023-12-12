19da79fa2bb50539738ebf9112391d3f9492e76b
Youssef Ben Yedder <youssefbenyedder04@gmail.com>
2023-12-12 05:44:24 -0500

    Did the 04 fetch data exercice

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,33 @@
-const url = 'https://api.github.com/users';
+import { useEffect } from "react";
+import { useState } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <h2>fetch data example</h2>
+      {users.map((user) => {
+        return (
+          <article key={user.id}>
+            <h3>{user.login}</h3>
+            <img src={user.avatar_url} alt={user.login} />
+            <div>
+              <a href={user.url}>{user.login}'s page.</a>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
 };
 export default FetchData;
