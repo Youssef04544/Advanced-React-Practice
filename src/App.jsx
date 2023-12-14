64854e33e10e9dbd2b61fa20ceca3e09a13796d3
Youssef Ben Yedder <youssefbenyedder04@gmail.com>
2023-12-14 02:51:19 -0500

    Did the toggle challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
+import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx";
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isVisible, setIsVisible] = useState(true);
+
+  const MagicalComponent = () => {
+    return <h4>So magical</h4>;
+  };
+
+  return (
+    <>
+      <h2>toggle challenge</h2>
+      <button className="btn" onClick={() => setIsVisible(!isVisible)}>
+        Toggle
+      </button>
+      {isVisible && <MagicalComponent />}
+    </>
+  );
 };
 
 export default ToggleChallenge;
