8ee8031476c33f0426c2cd7ce70a09baebcfe0ad
Youssef Ben Yedder <youssefbenyedder04@gmail.com>
2023-12-19 01:41:04 -0500

    user log in conditional rendering challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  const youssef = { name: "Youssef" };
+
+  const login = () => setUser(youssef);
+  const logout = () => setUser(null);
+
+  const LoggedIn = () => {
+    return (
+      <>
+        <h4>Hello there, {user.name}.</h4>
+        <button type="button" className="btn" onClick={logout}>
+          Log out
+        </button>
+      </>
+    );
+  };
+  const LoggedOut = () => {
+    return (
+      <>
+        <h4>Please log in.</h4>
+        <button type="button" className="btn" onClick={login}>
+          Log in
+        </button>
+      </>
+    );
+  };
+
+  return <>{user ? <LoggedIn /> : <LoggedOut />}</>;
 };
 
 export default UserChallenge;
